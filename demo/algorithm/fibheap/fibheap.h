1edb6ee82b7f4cb767b682fc1f651065b307d45b
Zhonqzmgming Qu <qzmfrank@umich.edu>
2014-12-11 14:08:48 -0500

    from home, minor

---
+++ demo/algorithm/fibheap/fibheap.h
@@ -0,0 +1,18 @@
+#ifndef _FIBHEAP_H_
+#define _FIBHEAP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+struct st_fibheap
+
+
+	
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* end of include guard */
--- demo/algorithm/lulu/quant_diagram/permute_threaded.cpp
+++ demo/algorithm/lulu/quant_diagram/permute_threaded.cpp
@@ -2,10 +2,13 @@
 #include <cstdlib>
 #include <assert.h>
 #include <algorithm>
+#include <list>
 #include <stack>
 #include <vector>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 
-using ::std::vector;
 using ::std::pair;
 using ::std::make_pair;
 
@@ -53,26 +56,55 @@ public:
 		static bool cmp(const pair<int,int> &lhs, const pair<int,int> &rhs)
 		{ return lhs.first < rhs.first; }
 	};
-public:
-	::std::stack<node*> _stack;
+private:
 	int _size;
+	size_t _count;
 public:
-	permute(const int size): _size(size)
-	{
-		assert( size % 2 == 0 );
-		node *tmp = new node;
-		tmp->group_list.push_back(make_pair(0, _size - 1));
-		_stack.push(tmp);
-	}
+	permute(const int size): _size(size), _count(0)
+	{ assert( size % 2 == 0   &&   size < 24); }
+
 	~permute() {}
 
-	bool empty() const { return _stack.empty(); }
+	bool empty() const { return _queue.empty(); }
 
-	void next()
+	void run(const int nthread)
 	{
-		assert(_stack.size());
-		node *p = _stack.top();
-		_stack.pop();
+		::std::list<node*> task_list;
+		_init_task_list(&task_list);
+
+		::std::mutex q_mtx;
+		::std::condition_variable q_cond;
+		auto *worker_list = new ::std::thread[n];
+		for (int i = 0; i < n; i++)
+			worker_list[i] = ::std::thread(_run_worker_function,
+					&q_mtx, &q_cond, &_queue);
+
+		while (1) {
+		}
+
+		for (int i = 0; i < n; i++)
+			worker_list[i].join();
+		delete []worker_list;
+	}
+
+private:
+	void _init_task_list(::std::list<node*> *task_list)
+	{
+		{
+		node *tmp = new node;
+		tmp->group_list.push_back(make_pair(0, _size - 1));
+		task_list->push_back(tmp);
+		}
+		const int TASK_LIST_MIN = 100;
+		while (1) {
+		}
+	}
+
+	void _add_to_task_list(::std::list<node*> *task_list)
+	{
+		assert(q->size());
+		node *p = q->front();
+		q->pop();
 
 		auto &gl = p->group_list;
 
@@ -95,7 +127,7 @@ public:
 			tmp->group_list.push_back(make_pair(i+1, end0));
 			for (int j = 1; j < gl.size(); j++)
 				tmp->group_list.push_back(gl[j]);
-			_stack.push(tmp);
+			q->push(tmp);
 		}
 		// edge case of the first group
 		if (end0 - start0 > 1) {
@@ -104,7 +136,7 @@ public:
 			tmp->edge_list.push_back(make_pair(start0, end0));
 			tmp->group_list = p->group_list;
 			tmp->group_list[0] = make_pair(start0+1, end0-1);
-			_stack.push(tmp);
+			q->push(tmp);
 		}
 
 		// branch the remaining groups, if any
@@ -126,7 +158,7 @@ public:
 					tmp->group_list.push_back(gl[k]);
 				for (int k = i+1; k < gl.size(); k++)
 					tmp->group_list.push_back(gl[k]);
-				_stack.push(tmp);
+				q->push(tmp);
 				continue;
 			}
 			// end > start
@@ -151,7 +183,96 @@ public:
 
 				for (int k = i+1; k < gl.size(); k++)
 					tmp->group_list.push_back(gl[k]);
-				_stack.push(tmp);
+				q->push(tmp);
+			}
+		}
+		delete p;
+	}
+
+	void _next_stack(::std::stack<node*> *s)
+	{
+		assert(s->size());
+		node *p = s->top();
+		s->pop();
+
+		auto &gl = p->group_list;
+
+		// acceptable state
+		if (gl.empty()) {
+			p->print();
+			delete p;
+			return;
+		}
+
+		// branch the first group
+		const int start0 = gl[0].first;
+		const int end0 = gl[0].second;
+		for (int i = start0 + 2; i < end0; i++) {
+			auto *tmp = new node;
+			tmp->edge_list = p->edge_list;
+			tmp->edge_list.push_back(make_pair(start0, i));
+			tmp->group_list.reserve(gl.size() + 1);
+			tmp->group_list.push_back(make_pair(start0+1, i-1));
+			tmp->group_list.push_back(make_pair(i+1, end0));
+			for (int j = 1; j < gl.size(); j++)
+				tmp->group_list.push_back(gl[j]);
+			s->push(tmp);
+		}
+		// edge case of the first group
+		if (end0 - start0 > 1) {
+			auto *tmp = new node;
+			tmp->edge_list  = p->edge_list;
+			tmp->edge_list.push_back(make_pair(start0, end0));
+			tmp->group_list = p->group_list;
+			tmp->group_list[0] = make_pair(start0+1, end0-1);
+			s->push(tmp);
+		}
+
+		// branch the remaining groups, if any
+		if (gl.size() == 1) {
+			delete p;
+			return;
+		}
+		for (int i = 1; i < gl.size(); i++) {
+			const int start = gl[i].first;
+			const int end = gl[i].second;
+			if (end - start == 0) {
+				auto *tmp = new node;
+				tmp->edge_list = p->edge_list;
+				tmp->edge_list.push_back(make_pair(start0, start));
+				tmp->group_list.reserve(gl.size() + 1);
+				if (end0 - start0)
+					tmp->group_list.push_back(make_pair(start0+1, end0));
+				for (int k = 1; k < i; k++)
+					tmp->group_list.push_back(gl[k]);
+				for (int k = i+1; k < gl.size(); k++)
+					tmp->group_list.push_back(gl[k]);
+				s->push(tmp);
+				continue;
+			}
+			// end > start
+			for (int j = start; j <= end; j++) {
+				auto *tmp = new node;
+				tmp->edge_list = p->edge_list;
+				tmp->edge_list.push_back(make_pair(start0, j));
+				tmp->group_list.reserve(gl.size() + 1);
+				if (end0 - start0)
+					tmp->group_list.push_back(make_pair(start0+1, end0));
+				for (int k = 1; k < i; k++)
+					tmp->group_list.push_back(gl[k]);
+
+				if (j == start) {
+					tmp->group_list.push_back(make_pair(start+1, end));
+				} else if (j == end) {
+					tmp->group_list.push_back(make_pair(start, end-1));
+				} else {
+					tmp->group_list.push_back(make_pair(start, j-1));
+					tmp->group_list.push_back(make_pair(j+1, end));
+				}
+
+				for (int k = i+1; k < gl.size(); k++)
+					tmp->group_list.push_back(gl[k]);
+				s->push(tmp);
 			}
 		}
 		delete p;
@@ -160,13 +281,13 @@ public:
 
 int main(int argc, char const* argv[])
 {
-	if (argc < 2) {
-		fprintf(stderr,"Usage: permute.exe [size]\n");
+	if (argc < 3) {
+		fprintf(stderr,"Usage: permute.exe [size] [nthread]\n");
 		exit(1);
 	}
 	const int size = ::std::atoi(argv[1]);
+	const int nthread = ::std::atoi(argv[2]);
 	permute g(size);
-	while (!g.empty())
-		g.next();
+	g.run(nthread);
 	return 0;
 }
