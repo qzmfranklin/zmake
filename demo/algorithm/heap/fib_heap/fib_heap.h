95f7bf5a328b1b703134ec9847bbbc826269509e
Zhongming Qu <qzmfranklin@gmail.com>
2014-12-12 18:14:39 -0500

    minor: debugging fib_heap

--- demo/algorithm/heap/fib_heap/fib_heap.h
+++ demo/algorithm/heap/fib_heap/fib_heap.h
@@ -7,8 +7,14 @@
 #include <limits.h>
 #include <string.h>
 #include <vector>
+#include <unordered_map>
 #include <cmath>
 #include "../heap.h"
+
+#define fprintf(stderr,...)
+
+using ::std::make_pair;
+using ::std::pair;
 
 template<typename T>
 class fib_heap: public heap<T> {
@@ -21,8 +27,14 @@ public:
 		node *child;
 		node *left;
 		node *right;
-		node(const T _key): key(_key), mark(false), degree(0),
-				parent(NULL), child(NULL) {}
+		node(const T &_key): key(_key), mark(false), degree(0),
+				parent(NULL), child(NULL), left(this), right(this) {}
+		void print_debug() const
+		{
+			fprintf(stderr,"\tleft = %d, child = %d, parent = %d, right = %d\n",
+					left->key, child ? child->key : 0,
+					parent ? parent->key : 0, right->key);
+		}
 		friend class fib_heap<T>;
 	};
 
@@ -32,6 +44,7 @@ private:
 	node *_last;
 	node **A; // auxilliary array used in _consolidate()
 	size_t A_size;
+	//::std::unordered_map<T,node*> _hash;
 
 public:
 	fib_heap():_size(0), _min(NULL), _last(NULL), A(NULL), A_size(0) { }
@@ -45,6 +58,7 @@ public:
 		if (!_min)
 			return;
 		node *z = _min;
+		//_hash.erase(z->key);
 		if (z->child) {
 			for (node *x = z->child->left; x != z->child; x = x->left)
 				_insert_left(z,x);
@@ -63,26 +77,88 @@ public:
 
 	virtual void push(const T &key)
 	{
+		fprintf(stderr,"\tpush(%d)\n",key);
 		auto *tmp = new fib_heap<T>::node(key);
+		//_hash.insert({{key,tmp}});
 		if (!_min)
 			_min = tmp;
 		else
 			_insert_left(_min,tmp);
+		if (key < _min->key)
+			_min = tmp;
 		_size++;
 		_last = tmp;
 	}
 
 	node *last() { return _last; }
 
-	void decrease_key(node *p, const T key);
+	void decrease_key(node *x, const T &key)
+	{
+		if (!(x->key < key))
+			return;
+		node *z = x;
+		x->key = key;
+		node *y = x->parent;
+		if (y  &&  x->key < y->key) {
+			while (1) {
+				_cut_subtree(x);
+				x = y;
+				y = y->parent;
+				if (!y)
+					break;
+				if (!x->mark) {
+					x->mark = true;
+					break;
+				}
+			}
+		}
+		if (z->key < _min->key)
+			_min = z;
+	}
 
 	// See Introduction to Algorithms 3rd Edition P523 for the max degree
 	size_t degree() const 
-	{ return (int) (::std::log(1.0*_size) / ::std::log(1.618) + 1.0E-6) + 1; }
+	{ return (size_t) (::std::log(1.0*_size) / ::std::log(1.618) + 1.0E-6) + 1; }
 
 private:
 	void _free()
 	{
+	}
+
+	// insert x to p's left
+	void _insert_left(node *p, node *x)
+	{
+		fprintf(stderr,"\t_insert_left([%3d] %p, [%3d] %p)\n",p->key,p,x->key,x);
+
+		/*
+		 *static int i = 0;
+		 *if (i++ > 40) {
+		 *        fprintf(stderr,"too many iterations, abort...\n");
+		 *        abort();
+		 *}
+		 */
+
+		node *q = p->left;
+		q->right = x;
+		p->left = x;
+		x->left = q;
+		x->right = p;
+		x->parent = p->parent;
+
+		p->print_debug();
+		x->print_debug();
+	}
+
+	// cut the tree rooted at @x and add it to the root list
+	void _cut_subtree(node *x)
+	{
+		x->left->right = x->right;
+		x->right->left = x->left;
+		x->mark = false;
+		if (x->parent->child == x)
+			x->parent->child = (x == x->right)  ?  NULL  :  x->right;
+		x->parent->degree--;
+		_insert_left(_min, x);
 	}
 
 	void _alloc_A()
@@ -155,26 +231,6 @@ private:
 		p->right->left = p->left;
 	}
 
-	// remove a node without children
-	void _remove_leaf(node *p)
-	{
-		p->left->right = p->right;
-		p->right->left = p->left;
-		if (p->parent)
-			if (p->parent->child == p)
-				p->parent->child = (p == p->right)  ?  p->child  :  p->right;
-	}
-
-	// insert x to p's left
-	void _insert_left(node *p, node *x)
-	{
-		node *q = p->left;
-		q->right = x;
-		p->left = x;
-		x->left = q;
-		x->right = p;
-		x->parent = p->parent;
-	}
 };
 
 #endif /* end of include guard */
--- demo/algorithm/heap/fib_heap/test_fib_heap.cpp
+++ demo/algorithm/heap/fib_heap/test_fib_heap.cpp
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <algorithm>
 #include "fib_heap.h"
 
 int main(int argc, char const* argv[])
 {
-	const int n = 30;
+	const int n = 10;
 	int a[n];
 	for (int i = 0; i < n; i++)
 		a[i] = rand() % (10 * n);
 	fib_heap<int> h;
 	for (int i = 0; i < n; i++)
 		h.push(a[i]);
+
+	::std::sort(a,a+n);
+	printf("Expected output:\n");
+	for (int i = 0; i < n; i++)
+		printf("%d\n",a[i]);
+	printf("\n");
+
+	printf("fib_heap output:\n");
 	for (int i = 0; i < n; i++) {
 		printf("%d\n",h.top());
 		h.pop();
