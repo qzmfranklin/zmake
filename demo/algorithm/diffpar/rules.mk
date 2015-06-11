53a7a4e66bb93c5d2a5492189f1af6e91650d32e
Zhongming Qu <qzmfrank@Zhongmings-MacBook-Pro.local>
2015-06-10 17:24:18 -0700

    Fix tab-space issue in Makefile. Add color to demo.

--- demo/algorithm/diffpar/rules.mk
+++ demo/algorithm/diffpar/rules.mk
@@ -15,17 +15,19 @@ ASM+=$($(DIR5a371ca09d660bb6863a788a9b8e6034)CPP:%.cpp=$(DIR5a371ca09d660bb6863a
 
 $(DIR5a371ca09d660bb6863a788a9b8e6034)/%.o: $(DIR5a371ca09d660bb6863a788a9b8e6034)/%.c
 	$(QUIET)$(CC) -o $@ -c $< $(DEPFLAGS) $($(DIR5a371ca09d660bb6863a788a9b8e6034)CFLAGS) $($(DIR5a371ca09d660bb6863a788a9b8e6034)INCS)
-	$(QUIET)echo "Compiling $(notdir $<) ..."
+	$(QUIET)echo "Compiling $(GREEN)$(notdir $<) $(NONE)..."
 $(DIR5a371ca09d660bb6863a788a9b8e6034)/%.s: $(DIR5a371ca09d660bb6863a788a9b8e6034)/%.c
 	$(QUIET)$(CC) -o $@ $< $(ASMFLAGS) $($(DIR5a371ca09d660bb6863a788a9b8e6034)CFLAGS) $($(DIR5a371ca09d660bb6863a788a9b8e6034)INCS)
+	$(QUIET)echo "Assembly listing $(CYAN)$(notdir $<) $(NONE)..."
 
 $(DIR5a371ca09d660bb6863a788a9b8e6034)/%.o: $(DIR5a371ca09d660bb6863a788a9b8e6034)/%.cpp
 	$(QUIET)$(CXX) -o $@ -c $< $(DEPFLAGS) $($(DIR5a371ca09d660bb6863a788a9b8e6034)CXXFLAGS) $($(DIR5a371ca09d660bb6863a788a9b8e6034)INCS)
-	$(QUIET)echo "Compiling $(notdir $<) ..."
+	$(QUIET)echo "Compiling $(GREEN)$(notdir $<) $(NONE)..."
 $(DIR5a371ca09d660bb6863a788a9b8e6034)/%.s: $(DIR5a371ca09d660bb6863a788a9b8e6034)/%.cpp
 	$(QUIET)$(CXX) -o $@ $< $(ASMFLAGS) $($(DIR5a371ca09d660bb6863a788a9b8e6034)CXXFLAGS) $($(DIR5a371ca09d660bb6863a788a9b8e6034)INCS)
+	$(QUIET)echo "Assembly listing $(CYAN)$(notdir $<) $(NONE)..."
 
 # Linking pattern rule for this directory
 %.exe: $(DIR5a371ca09d660bb6863a788a9b8e6034)/%.o
 	$(QUIET)$(CXX) -o $@ $^ $($(DIR5a371ca09d660bb6863a788a9b8e6034)LIBS)
-	$(QUIET)echo "Linking $(notdir $@) ..."
+	$(QUIET)echo "Linking $(MAGENTA)$(notdir $@) $(NONE)..."
